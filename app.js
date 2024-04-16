f2684e5fa5c0e2b6c1cf514e2d1d23c149fde732
Nicholas <nicholaspriegel@gmail.com>
2024-04-16 14:00:50 -0400

    added variables for boundaries

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ let arrowDown = false;
 let arrowLeft = false;
 let arrowRight = false;
 
+const topBoundry = -4;
+const bottomBoundry = 661;
+const leftBoundry = -2;
+const rightBoundry = 428;
+
 // Update arrow key states
 function updateArrowKeys(key, status) {
   switch (key) {
@@ -38,25 +43,25 @@ function updateArrowKeys(key, status) {
 }
 
 function moveCar() {
-    if (arrowUp && arrowLeft && posY > -4 && posX > -2) {
+    if (arrowUp && arrowLeft && posY > topBoundry && posX > leftBoundry) {
         posY -= 7;
         posX -= 7;
-    } else if (arrowUp && arrowRight && posY > -4 && posX < 428) {
+    } else if (arrowUp && arrowRight && posY > topBoundry && posX < rightBoundry) {
         posY -= 7;
         posX += 7;
-    } else if (arrowDown && arrowLeft && posX > -2 && posY < 661) {
+    } else if (arrowDown && arrowLeft && posX > leftBoundry && posY < bottomBoundry) {
         posY += 7;
         posX -= 7;
-    } else if (arrowDown && arrowRight && posX < 428 && posY < 661) {
+    } else if (arrowDown && arrowRight && posX < rightBoundry && posY < bottomBoundry) {
         posY += 7;
         posX += 7;
-    } else if (arrowUp && posY > -4){
+    } else if (arrowUp && posY > topBoundry){
         posY -= 5;
-    } else if (arrowDown && posY < 661){
+    } else if (arrowDown && posY < bottomBoundry){
         posY += 5;
-    } else if (arrowLeft && posX > -2){
+    } else if (arrowLeft && posX > leftBoundry){
         posX -= 5;
-    } else if (arrowRight && posX < 428){
+    } else if (arrowRight && posX < rightBoundry){
         posX += 5;
     }
 
@@ -77,8 +82,3 @@ document.addEventListener('keydown', function(event) {
 document.addEventListener('keyup', function(event) {
     updateArrowKeys(event.key, false);
 });
-
-// Event listener for keyboard arrow keys
-// document.addEventListener('keydown', function(event) {
-//   moveCar(event.key);
-// });
